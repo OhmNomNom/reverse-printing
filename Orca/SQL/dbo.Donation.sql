a2d65ddf6afeec46d09a2fbed305e0f13bc4143e
OhmNomNom <samer.abuthaher@gmail.com>
2015-03-28 14:00:37 +0200

    Include DB structure in repo

---
+++ Orca/SQL/dbo.Donation.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Donation] (
+    [ID]        INT             IDENTITY (1, 1) NOT NULL,
+    [AUBnet]    CHAR (8)        NOT NULL,
+    [Kilos]     DECIMAL (18, 3) NOT NULL,
+    [Processed] BIT             DEFAULT ((0)) NOT NULL,
+    [Actor]     CHAR (8)        NOT NULL,
+    [Timestamp] DATETIME        DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [Processed]
+    ON [dbo].[Donation]([Processed] ASC);
+
---
+++ Orca/SQL/dbo.Reward.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Reward] (
+    [Id]     INT             IDENTITY (1, 1) NOT NULL,
+    [Major]  NCHAR (16)      NOT NULL,
+    [Factor] DECIMAL (18, 3)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [Major_Index]
+    ON [dbo].[Reward]([Major] ASC);
+
